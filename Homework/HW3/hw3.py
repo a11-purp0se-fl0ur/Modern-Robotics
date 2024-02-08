a08193ca9a2c9ca823c11445c55ea0dd47f81c8b
a11-purp0se-fl0ur <mia-scoblic@outlook.com>
2024-02-08 17:31:44 -0600

    Finished homework 3

--- Homework/HW3/hw3.py
+++ Homework/HW3/hw3.py
@@ -5,21 +5,61 @@ Date: 2024-02-03
 """
 
 import numpy as np
-from Functions.Mia_Functions import *
-from Functions.Phil_Functions import *
+
+# Phil's Rot Function --------------------------------------------------------------------------------------------------
+def Rot(axis, angle, ang_type):
+    '''
+    Description: Calculates the rotation matrix for a given direction and angle.
+    Input: A rotation axis, angle and angle type, axis, angle, ang_type, respectively.
+    Return: The rotation matrix describing the current configuration
+    Example Input:
+        axis = 'z'
+        angle = 30
+        ang_type = 'deg'
+    Output:
+        np.array([[ 0, -3,  2],
+                  [ 3,  0, -1],
+                  [-2,  1,  0]])
+    '''
+    ang_type = ang_type.upper()
+
+    if ang_type == 'DEG':
+        angle = np.radians(angle)
+
+    if axis == 'x':
+        R = np.array([[1, 0, 0],
+                      [0, np.cos(angle), -np.sin(angle)],
+                      [0, np.sin(angle), np.cos(angle)]
+                      ])
+    elif axis == 'y':
+        R = np.array([[np.cos(angle), 0, np.sin(angle)],
+                      [0, 1, 0],
+                      [-np.sin(angle), 0, np.cos(angle)]
+                      ])
+    elif axis == 'z':
+        R = np.array([[np.cos(angle), -np.sin(angle), 0],
+                      [np.sin(angle), np.cos(angle), 0],
+                      [0, 0, 1]
+                      ])
+    else:
+        raise NameError('Valid axis not provided. Options are: x, y or z.')
+
+    return R
 
 # Problem 1 ------------------------------------------------------------------------------------------------------------
 print('\nProblem 1:')
 x_a = np.array([0, 0, 1])
 y_a = np.array([-1, 0, 0])
-Rsa = s_a(x_a, y_a)
+z_a = np.cross(x_a, y_a)
+Rsa = np.column_stack((x_a, y_a, z_a))
 print('Rsa:\n', Rsa)
 
 # Problem 2 ------------------------------------------------------------------------------------------------------------
 print('\nProblem 2:')
 x_b = np.array([1, 0, 0])
 y_b = np.array([0, 0, -1])
-Rsb = s_a(x_b, y_b)
+z_b = np.cross(x_b, y_b)
+Rsb = np.column_stack((x_b, y_b, z_b))
 print('Rsb:\n', Rsb)
 
 # Problem 3 ------------------------------------------------------------------------------------------------------------
@@ -30,7 +70,7 @@ print('Rbs:\n', Rbs)
 # Problem 4 ------------------------------------------------------------------------------------------------------------
 print('\nProblem 4:')
 Ras = np.transpose(Rsa)
-Rab = a_b(Ras, Rsb)
+Rab = Ras @ Rsb
 print('Rab:\n', Rab)
 
 # Problem 5 ------------------------------------------------------------------------------------------------------------
@@ -68,7 +108,7 @@ X = np.round(Rab_2, 1)
 print('Rbs:\n', X)
 
 # Problem 11 -----------------------------------------------------------------------------------------------------------
-print('\nProblem 10:')
+print('\nProblem 11:')
 Rba_2 = np.transpose(X)
 print('Rba:\n', Rba_2)
 
