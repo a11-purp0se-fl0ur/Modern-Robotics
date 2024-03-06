eb40aaccc9bedaaf6b360116c8c86eb6d5518d28
Mia <mia-scoblic@outlook.com>
2024-03-06 13:51:06 -0600

    Begin Exam 1 set up

---
+++ Exams/Exam 1/Exam1.py
@@ -0,0 +1,27 @@
+"""
+Description: ME:4140 Exam 1
+Name: Mia Scoblic
+Date: 3/6/2024
+"""
+
+# Packages and Rounding ------------------------------------------------------------------------------------------------
+from Functions.Mia_Functions import *
+
+dec = 3
+np.set_printoptions(precision=3, suppress=True)
+# ----------------------------------------------------------------------------------------------------------------------
+
+# NOTE: I will be importing my functions as seen above. I may be pasting the applicable functions below to allow you to
+#       view my functions without downloading a separate functions file. (I'm not sure how to share the functions if you
+#       don't have the "Functions" folder as I do. I'll attach it to ICON either way.
+
+# Pasted Functions -----------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# EXAM BEGIN -----------------------------------------------------------------------------------------------------------
+
+# Problem 1 ------------------------------------------------------------------------------------------------------------
+print("\nProblem 1:\n")
+
+# ----------------------------------------------------------------------------------------------------------------------
